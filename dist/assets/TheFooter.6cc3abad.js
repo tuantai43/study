e27b14d996f5e555ba66156ef1c082bc83c8d7d5
Pham Tuan Tai <taipt22@fpt.com.vn>
2021-10-08 17:18:17 +0700

    adding dist

---
+++ dist/assets/TheFooter.6cc3abad.js
@@ -0,0 +1,2 @@
+import{_ as r}from"./index.d09b7d6c.js";import"./vendor.d2d6052d.js";const e={};function o(t,d,s,n,c,_){return"Footer"}var f=r(e,[["render",o]]);export{f as default};
+//# sourceMappingURL=TheFooter.6cc3abad.js.map
