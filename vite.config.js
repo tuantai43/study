66da33a79c4251a61aa4522c733fc08929ac1080
Pham Tuan Tai <taipt22@fpt.com.vn>
2021-10-08 16:28:34 +0700

    update UI

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/main.js"),
-      name: "MyCssLib",
+    plugins: [vue()],
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, 'src/main.js'),
+            name: 'MyCssLib',
+        },
     },
-  },
-})
+});
