617054aabbaad234e6aa844766aa5171e9fefb47
Pham Tuan Tai <taipt22@fpt.com.vn>
2021-10-11 23:56:45 +0700

    add .env

--- src/repositories/repository.js
+++ src/repositories/repository.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseDomain = 'http://localhost:3000';
+const baseDomain = import.meta.env.VITE_APP_NAME;
 const baseUrl = `${baseDomain}/api`;
 
 export default axios.create({
