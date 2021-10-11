ee3d0d021f0b57e0ac226b11509bed3e8fa01f44
Pham Tuan Tai <taipt22@fpt.com.vn>
2021-10-11 14:28:44 +0700

    adding dist

---
+++ assets/TheFooter.0abe47cd.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.b242487a.js";import{c as o,o as r}from"./vendor.407f7b89.js";const t={},a={class:"footer"};function c(s,n){return r(),o("div",a,"\xA92021 pham tuan tai")}var i=e(t,[["render",c]]);export{i as default};
